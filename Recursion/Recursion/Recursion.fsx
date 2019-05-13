a33fbbe7c2ae4850dfe202045338fe1660a78c1a
dianaliz93 <djard005@fiu.edu>
2019-05-12 21:34:38 -0400

    Added fibinacci to recursion

--- Recursion/Recursion/Recursion.fsx
+++ Recursion/Recursion/Recursion.fsx
@@ -1,5 +1,5 @@
 ﻿let rec fact = function
-    |1 ->1
+    |1 -> 1
     |n ->n * fact(n - 1)
 
 fact 4
@@ -7,10 +7,15 @@ fact 4
 //Using else if statement
 
 let rec fact2 n = 
-    if 
-        n <= 1 then 1
-    else 
-        n * fact2(n - 1)
+    if n <= 1 then 1
+    else n * fact2(n - 1)
 
 fact2 4
 fact2 1
+
+let rec fib = function
+    |0 -> 0I
+    |1 -> 1I
+    |n -> fib(n - 1) + fib(n - 2)
+
+fib 4
