86494b09b41749f0efc626e73735470863784fb1
dianaliz93 <djard005@fiu.edu>
2019-05-13 06:34:11 -0400

    Problem 16, defining infix operators for addition and multiplication of fractions, however not giving me the same answer as in the example

---
+++ ProblemSet1/ProblemSet1/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace ProblemSet1.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("ProblemSet1")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("ProblemSet1")>]
+[<assembly: AssemblyCopyright("Copyright ©  2019")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("ac75c22f-9eae-47f2-890e-c2f6ee7e528d")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ ProblemSet1/ProblemSet1/Problem16.fsx
@@ -0,0 +1,20 @@
+﻿//Problem 16
+
+let rec gcd = function 
+    | (a, 0) -> a
+    | (a, b) -> gcd(b, a % b);;
+
+let (.+) (a, b) (c, d) = 
+    let x = (a * d) + (b * c)
+    let y = (b * d)
+    let z = gcd(a, b)
+    (x/c, y/c);;
+ 
+let (.*) (a, b) (c, d) = 
+    let x = (a * c)
+    let y = (b * d)
+    let z = gcd(x, y)
+    (x/c, y/c);;
+
+(1, 2) .+ (1, 2);;
+(1,2) .+ (2,3) .* (3,7);;
